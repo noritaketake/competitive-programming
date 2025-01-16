39ff0fa5d224040c2dc8579167ecf57d1770c645
noritotakeda <noritotakeda@gmail.com>
2025-01-16 11:29:25 +0900

    16th

--- atcoder/abc365/c.php
+++ atcoder/abc365/c.php
@@ -9,5 +9,28 @@ function yes() { echo 'Yes', PHP_EOL;}
 function no() { echo 'No', PHP_EOL;}
 function p($val) { echo $val, PHP_EOL; }
 function mid($l, $r) { return intval(floor(($l + $r) / 2)); } // for binary search
-function sum($arr) { return array_reduce($arr, fn($total, $curr) => $total + $curr, 0); }
+function sum(array $arr) { return array_reduce($arr, fn($total, $curr) => $total + $curr, 0); }
 const EXPO_9 = 1000_000_000;
+
+[$n, $m] = ia();
+$a = ia();
+
+$sum = sum($a);
+if ($sum <= $m) return p("infinite");
+
+sort($a);
+
+$ok = 0;
+$ng = EXPO_9 + 1;
+while (abs($ok - $ng) > 1) {
+    $mid = mid($ok, $ng);
+
+    $tmp_sum = 0;
+    foreach ($a as $num) {
+        $tmp_sum += min($mid, $num);
+    }
+
+    if ($tmp_sum <= $m) $ok = $mid;
+    else $ng = $mid;
+}
+p($ok);
